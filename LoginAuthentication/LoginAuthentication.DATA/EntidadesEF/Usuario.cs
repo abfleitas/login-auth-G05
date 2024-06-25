420b34663e176ef55ed4e54b7fa9f7885791ab2a
AlanForno <aforno@alumno.unlam.edu.ar>
2024-06-25 15:38:38 -0300

    Arreglo rol null en db

--- LoginAuthentication/LoginAuthentication.DATA/EntidadesEF/Usuario.cs
+++ LoginAuthentication/LoginAuthentication.DATA/EntidadesEF/Usuario.cs
@@ -10,7 +10,7 @@ namespace LoginAuthentication.DATA.EntidadesEF
         public string Mail { get; set; } = null!;
         public string Username { get; set; } = null!;
         public string Password { get; set; } = null!;
-        public string? Rol { get; set; }
+        public string? Rol { get; set; } = "Usuario";
         public bool? EmailVerificado { get; set; }
     }
 }
